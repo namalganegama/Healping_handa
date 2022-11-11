4f03e8cb928c235cf9e3ea54fa77c2caf25edff2
namal <namalpathum28@gmail.com>
2022-11-11 15:39:22 +0530

    beneficery function implementation

---
+++ constants/styles.js
@@ -0,0 +1,28 @@
+export const GlobalStyles = {
+    colors: {
+      primary50: '#e4d9fd',
+      primary100: '#c6affc',
+      primary200: '#a281f0',
+      primary300: '#B3FFAE',
+      primary400: '#5721d4',
+      primary500: '#3e04c3',
+      primary600: '#323232',
+      primary700: '#2d0689',
+      primary800: '#200364',
+      primary900: '#F5EEDC',
+      accent500: '#f7bc0c',
+      error50: '#fcc4e4',
+      error500: '#9b095c',
+      gray500: '#39324a',
+      gray700: '#221c30',
+      black: '#393E46',
+      yellow: '#F2E7D5',
+      green: '#B3FFAE',
+      white: '#EAEAEA',
+      red: '#FF6464',
+      rose: '#FF9494',
+      blue: '#C3F8FF',
+      purple: '#533E85'
+
+    },
+  };
---
+++ screens/AllExpenses.js
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import { ExpensesContext } from '../store/expenses-context';
+
+function AllExpenses() {
+  const expensesCtx = useContext(ExpensesContext);
+
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Donation Count"
+      fallbackText="No registered Beneficaries found!"
+    />
+  );
+}
+
+export default AllExpenses;
